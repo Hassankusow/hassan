1fdef81134cda6511cc4742397ba0519e5778e4b
Hassankusow <149752029+Hassankusow@users.noreply.github.com>
2024-02-12 18:56:25 -0800

    getopt.h

--- getopt.h
+++ getopt.h
@@ -7,8 +7,3 @@ struct option {
         int         val;
 };
 
-void float_to_hex(float value);
-void double_to_hex(double value);
-void hex_to_float(const char *hex_string);
-void hex_to_double(const char *hex_string);
-void print_double(double value);
